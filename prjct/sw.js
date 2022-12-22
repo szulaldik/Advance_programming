b3c0536d5eab54c07f7d5e6a88c82b3ea6cf650b
Sümeyye Zülal Dik <115083072+szulaldik@users.noreply.github.com>
2022-12-22 12:24:45 +0300

    Add files via upload

---
+++ prjct/sw.js
@@ -0,0 +1,22 @@
+const CACHE ='prjct'
+const FILES = [ 'index.html', 'images/flying-star.png', 'images/bear.png', 'images/bee.png',
+'images/bird.png', 'images/cat.png', 'images/corgi.png', 'images/flamingo.png', 'images/penguin.png',
+'images/owl.png', 'images/soru.jpg', 'images/clown-fish.png', 'images/lion.png','./']
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+
+self.addEventListener('install', installCB)
+function cacheCB(e) { //cache first
+  let req = e.request
+  e.respondWith(
+    caches.match(req)
+    .then(r1 => r1 || fetch(req))
+    .catch(console.log)
+  )
+}
+self.addEventListener('fetch', cacheCB)

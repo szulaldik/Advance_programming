90e1088e1cb20933f46a8a4c3187e4bc905628ea
Sümeyye Zülal Dik <115083072+szulaldik@users.noreply.github.com>
2022-12-23 13:39:57 +0300

    Update sw.js

--- prjct/sw.js
+++ prjct/sw.js
@@ -1,4 +1,5 @@
 const CACHE ='prjct'
+
 const FILES = [ 'prjct/index.html', 'prjct/images/flying-star.png', 'prjct/images/bear.png', 'prjct/images/bee.png',
 'prjct/images/bird.png', 'prjct/images/cat.png', 'prjct/images/corgi.png', 'prjct/images/flamingo.png', 'prjct/images/penguin.png',
 'prjct/images/owl.png', 'prjct/images/soru.jpg', 'prjct/images/clown-fish.png', 'prjct/images/lion.png', 'prjct/manifest.json','./']
@@ -10,14 +11,20 @@ function installCB(e) {
     .catch(console.log)
   )
 }
-
 self.addEventListener('install', installCB)
-function cacheCB(e) { //cache first
+function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  }) 
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
   let req = e.request
   e.respondWith(
-    caches.match(req)
-    .then(r1 => r1 || fetch(req))
-    .catch(console.log)
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
   )
 }
-self.addEventListener('fetch', cacheCB)
+self.addEventListener('fetch', fetchCB)
